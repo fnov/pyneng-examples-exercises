8a324f61befc46b46961a8e043b9ab89f165eb8a
sm1Le <sm1Le@outlook.com>
2026-02-17 22:20:52 +0300

    pyneng task_12_1

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -17,3 +17,33 @@ IP-адрес считается доступным, если выполнени
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+######################
+import subprocess
+
+
+def ping_ip_addresses(ips: list) -> tuple:
+    abailable_ips = []
+    unavailable_ips = []
+    for ip in ips:
+       if run_command(f'ping -c 1 {ip}'):
+           abailable_ips.append(ip)
+       else:
+           unavailable_ips.append(ip)
+    return abailable_ips, unavailable_ips
+
+
+def run_command(command):
+    result = subprocess.run(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding='utf-8')
+    if result.returncode == 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    ping_ip_addresses(['8.8.8.8', '1.1.1.1', '10.255.255.254' ])
